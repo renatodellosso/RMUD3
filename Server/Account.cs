0ec50220437268319f9d20863b92460f8ee65984
Renato Dell'Osso <54045197+renatodellosso@users.noreply.github.com>
2024-04-18 22:02:48 -0400

    Hash unit tests

--- Server/Account.cs
+++ Server/Account.cs
@@ -14,25 +14,26 @@ namespace RMUD3.Server
 			salt = GenerateSalt();
 		}
 
-		public static string Hash(string password, byte[] salt, byte[] pepper, IEnvService? envService = null)
+		private static string Hash(string password, byte[] salt, IEnvService? envService = null)
 		{
 			envService ??= Services.GetEnv();
 
 			ArgumentNullException.ThrowIfNull(envService);
 
-			Argon2id argon = new(Encoding.UTF8.GetBytes(password));
+			Argon2id argon = new(Encoding.UTF8.GetBytes(password))
+			{
+				Salt = salt,
+				KnownSecret = envService.Pepper,
 
-			argon.Salt = salt;
-			argon.KnownSecret = pepper;
-
-			argon.DegreeOfParallelism = envService.Parallelism;
-			argon.MemorySize = envService.Memory;
-			argon.Iterations = envService.Iterations;
+				DegreeOfParallelism = envService.Parallelism,
+				MemorySize = envService.Memory,
+				Iterations = envService.Iterations
+			};
 
 			return Convert.ToBase64String(argon.GetBytes(32));
 		}
 
-		public static byte[] GenerateSalt()
+		private static byte[] GenerateSalt()
 		{
 			var buffer = new byte[32];
 			RandomNumberGenerator.Create().GetNonZeroBytes(buffer);
--- Testing/Tests/AccountTests.cs
+++ Testing/Tests/AccountTests.cs
@@ -1,4 +1,5 @@
 ﻿using RMUD3.Server;
+using System.Reflection;
 using System.Text;
 
 namespace Testing.Tests
@@ -6,10 +7,161 @@ namespace Testing.Tests
 	[TestClass]
 	public class AccountTests
 	{
+
+		private static readonly byte[] pepper = Encoding.UTF8.GetBytes("pepper");
+
+		private const int PARALLELISM = 4, ITERATIONS = 10;
+		private const string MEMORY = "25KB";
+		private static IEnvService GenerateEnv(int parallelism = PARALLELISM, int iterations = ITERATIONS, string memory = MEMORY, string? pepper = null)
+			=> TestUtils.GenerateEnv($"PARALLELISM={parallelism}\nITERATIONS={iterations}\nMEMORY={memory}\nPEPPER={pepper ?? Convert.ToBase64String(AccountTests.pepper)}");
+
+		private static byte[]? GenerateSalt() => (byte[]?)typeof(Account).GetMethod("GenerateSalt", BindingFlags.NonPublic | BindingFlags.Static)?.Invoke(null, null);
+		private static string? Hash(string password, byte[] salt, IEnvService env)
+			=> (string?)typeof(Account).GetMethod("Hash", BindingFlags.NonPublic | BindingFlags.Static)?.Invoke(null, [password, salt, env]);
+
 		[TestMethod]
-		public void Hash_ValidFakeConfig_Success()
+		public void Hash_FakeConfig_Success()
 		{
-			Account.Hash("password", Account.GenerateSalt(), Encoding.UTF8.GetBytes("pepper"), TestUtils.GenerateEnv("PARALLELISM=4\nITERATIONS=4\nMEMORY=4KB"));
+			byte[]? salt = GenerateSalt();
+			if (salt == null)
+				Assert.Fail("Salt is null");
+
+			string? hash = Hash("password", salt, GenerateEnv());
+
+			if (hash == null)
+				Assert.Fail("Hash is null");
+
+			Assert.AreEqual(44, hash.Length);
+		}
+
+		[TestMethod]
+		public void Hash_MultiDifferentPassword_Success()
+		{
+			byte[]? salt = GenerateSalt();
+			if (salt == null) Assert.Fail("Salt is null");
+
+			// Generate 2 hashes
+			IEnvService envService = GenerateEnv();
+			string? hash1 = Hash("password1", salt, envService);
+			string? hash2 = Hash("password2", salt, envService);
+
+			if (hash1 == null) Assert.Fail("Hash1 is null");
+			if (hash2 == null) Assert.Fail("Hash2 is null");
+
+			Assert.AreNotEqual(hash1, hash2);
+		}
+
+		[TestMethod]
+		public void Hash_MultiDifferentSalt_Success()
+		{
+			byte[]? salt1 = GenerateSalt();
+			if (salt1 == null) Assert.Fail("Salt1 is null");
+
+			byte[]? salt2 = GenerateSalt();
+			if (salt2 == null) Assert.Fail("Salt2 is null");
+
+			// Generate 2 hashes
+			IEnvService envService = GenerateEnv();
+			string? hash1 = Hash("password", salt1, envService);
+			string? hash2 = Hash("password", salt2, envService);
+
+			if (hash1 == null) Assert.Fail("Hash1 is null");
+			if (hash2 == null) Assert.Fail("Hash2 is null");
+
+			Assert.AreNotEqual(hash1, hash2);
+		}
+
+		[TestMethod]
+		public void Hash_MultiDifferentPepper_Success()
+		{
+			byte[]? salt = GenerateSalt();
+			if (salt == null) Assert.Fail("Salt is null");
+
+			string? hash1 = Hash("password", salt, GenerateEnv());
+			string? hash2 = Hash("password", salt, GenerateEnv(pepper: "pepper2"));
+
+			if (hash1 == null) Assert.Fail("Hash1 is null");
+			if (hash2 == null) Assert.Fail("Hash2 is null");
+
+			Assert.AreNotEqual(hash1, hash2);
+		}
+
+		[TestMethod]
+		public void Hash_MultiDifferentParallelism_Success()
+		{
+			byte[]? salt = GenerateSalt();
+			if (salt == null) Assert.Fail("Salt is null");
+
+			// Generate 2 hashes
+			IEnvService envService1 = GenerateEnv();
+
+			IEnvService envService2 = GenerateEnv(PARALLELISM + 1);
+
+			string? hash1 = Hash("password", salt, envService1);
+			string? hash2 = Hash("password", salt, envService2);
+
+			if (hash1 == null) Assert.Fail("Hash1 is null");
+			if (hash2 == null) Assert.Fail("Hash2 is null");
+
+			Assert.AreNotEqual(hash1, hash2);
+		}
+
+		[TestMethod]
+		public void Hash_MultiDifferentIterations_Success()
+		{
+			byte[]? salt = GenerateSalt();
+			if (salt == null) Assert.Fail("Salt is null");
+
+			// Generate 2 hashes
+			IEnvService envService1 = GenerateEnv();
+
+			IEnvService envService2 = GenerateEnv(iterations: ITERATIONS + 1);
+
+			string? hash1 = Hash("password", salt, envService1);
+			string? hash2 = Hash("password", salt, envService2);
+
+			if (hash1 == null) Assert.Fail("Hash1 is null");
+			if (hash2 == null) Assert.Fail("Hash2 is null");
+
+			Assert.AreNotEqual(hash1, hash2);
+		}
+
+		[TestMethod]
+		public void Hash_MultiDifferentMemory_Success()
+		{
+			byte[]? salt = GenerateSalt();
+			if (salt == null) Assert.Fail("Salt is null");
+
+			// Generate 2 hashes
+			IEnvService envService1 = GenerateEnv();
+
+			// Increment memory by 1 by parsing the first character as an int and then incrementing it
+			IEnvService envService2 = GenerateEnv(memory: $"{int.Parse(MEMORY[0].ToString()) + 1}KB");
+
+			string? hash1 = Hash("password", salt, envService1);
+			string? hash2 = Hash("password", salt, envService2);
+
+			if (hash1 == null) Assert.Fail("Hash1 is null");
+			if (hash2 == null) Assert.Fail("Hash2 is null");
+
+			Assert.AreNotEqual(hash1, hash2);
+		}
+
+		[TestMethod]
+		public void Hash_RealEnv_Success()
+		{
+			byte[]? salt = GenerateSalt();
+			if (salt == null) Assert.Fail("Salt is null");
+
+			// Generate 2 hashes
+			string filePath = "../../../../.env";
+			IEnvService envService = new EnvService(filePath);
+
+			string? hash = Hash("password", salt, envService);
+
+			if (hash == null) Assert.Fail("Hash is null");
+
+			Assert.AreEqual(44, hash.Length);
 		}
 	}
 }
